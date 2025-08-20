89137c21bda8893c5b7e5106cb6e5d2f8fe74683
Shivani Naroju <142157246+Shivaninaroju@users.noreply.github.com>
2025-08-20 21:17:38 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
+
 # Get API Keys
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 UNSPLASH_ACCESS_KEY = os.getenv("UNSPLASH_ACCESS_KEY")
